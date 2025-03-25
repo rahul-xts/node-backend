1ba70bdc8ee9676070a9f38bc738ebe2b35e263a
Rahul Tupparwar <aphatak@ondot.com>
2025-03-25 17:34:14 +0530

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 console.log("hello i am here");
-app.get('/', (req, res) => res.send('Hello from git -> jenkins -> Node.js Backend 2025'));
+app.get('/', (req, res) => res.send('Hello from git -> jenkins -> Node.js Backend 2025 thanks'));
 app.listen(3000, () => console.log('Server running on port 3000')); 
