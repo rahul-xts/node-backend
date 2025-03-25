a56e9bb7cf3ea60839ec80ca37d7aa9706a806d3
Rahul Tupparwar <aphatak@ondot.com>
2025-03-25 14:49:47 +0530

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
+console.log("hello i am here");
 app.get('/', (req, res) => res.send('Hello from Node.js Backend'));
 app.listen(3000, () => console.log('Server running on port 3000'));
